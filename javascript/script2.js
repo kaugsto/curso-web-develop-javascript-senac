87566178cdb5fc1f3cb709d0c595e3eda63c9d09
kleber <k.augsto@gmail.com>
2023-01-26 12:30:41 -0300

    Adicionado console log ({email, groups, etc

--- javascript/script2.js
+++ javascript/script2.js
@@ -11,5 +11,20 @@ console.log ("thisVar:", thisVar, "and thatVar:", thatVar);
 var greet = "Hello", who ="World";
 console.log("%s, %s!", greet, who);
 
+console.log({
+    'Email': '',
+    'Groups': {},
+    'Id': 33,
+    'IsSieAdmin': false,
+    'IsHiddenInUI': false,
+    'LoginName':'i:0#.w|virtualdomain\\user2',
+    'PrincipalType':1,
+    'Title': 'user2',
+})
 
+console.log ("Hello World!");   
+var x = 10;
+var y = 20;
+var sum = x + y;
+console.log(sum);
 
