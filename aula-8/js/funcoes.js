15b4956a176cdc8e3ca64271fa6c1a78c641284d
Kleber Augusto <103510713+kaugsto@users.noreply.github.com>
2023-02-14 17:44:45 -0300

    Add files via upload

---
+++ aula-8/js/funcoes.js
@@ -0,0 +1,76 @@
+//Função que não retorna
+
+var x;
+function soma(array_numeros){
+
+    var rs = 0;
+    for ( var i =0 ; i < array_numeros.length ; i++){
+        rs += array_numeros[i];
+    }
+
+    console.log(rs);
+
+}
+
+function maior (array_numeros){ 10-30-2-13-45
+
+    var m = array_numeros[0];
+    for (var i = 1 ; i <array_numeros.length ; i++) {
+
+        if (array_numeros[i] > m){
+            m = array_numeros[i];
+        }
+    }
+    console.log(m);
+
+}
+
+function menor (array_numeros){ 10-30-2-13-45
+
+    var m = array_numeros[0];
+    for (var i = 1 ; i <array_numeros.length ; i++) {
+
+        if (array_numeros[i] < m){
+            m = array_numeros[i];
+        }
+    }
+    console.log(m);
+
+}
+
+// Funções com retorno
+
+function mensagem(nome){
+    return `Olá sr(a). ${nome}`;
+};
+
+const msg = function (nome){
+    return `Olá sr(a). ${nome}`;
+};
+
+const anobissexto = function (ano){
+    if (ano % 4 == 0){
+        return "ano bissexto";
+    }else{
+        return "ano não bissexto";
+    }
+};
+
+const paletaCores = function(linha,coluna){
+
+    var tabela = "<table>";
+    
+}   
+
+for (var lin = 0; lin < linha; lin++){
+    
+    tabela += "<tr>";
+    for (var col = 0; col < coluna; col++){
+        var red = Math.round (1 + Math.random() * 254);
+        var green = Math.round (1 + Math.random() * 254);
+        var blue = Math.round (1 + Math.random() * 254);
+        tabela += `<td style ='background-color=rgb(${red},${green},${blue})'>_</td>`;
+    }
+    tabela += "</tr>";
+}
+return tabela;
---
+++ aula-8/js/script.js
@@ -0,0 +1,51 @@
+window.document.body.onload = function(){
+    //Vamos chamar a função soma com a passagem de 
+    //numeros de um array
+
+    var numeros = [10, 30, 5, 8, 20];
+    soma(numeros);
+
+    // var valores = prompt("Digite números separados por vírgula");
+    
+    
+    //O comando split aplicado a variavel valores é capaz de quebrar os números digitados pelo usuário
+    //onde há vírgula e assim gerar um array com esses números.
+
+
+    // var n = valores.split(",");
+    // console.log(n);
+    // var np = [];
+    //     for (var i = 0 ; i <n.length ; i++) {
+    //         np.push(parseInt(n[i]));
+    //     }
+    
+    // console.log(np);
+    // maior(np);
+    // menor(np);
+
+    const inputNome = document.createElement("input");
+    inputNome.setAttribute ("placeholder", "Digite seu nome");
+    inputNome.onblur = function () {
+        inputNome.value = mensagem(inputNome.value);
+        console.log(mensagem(inputNome.value));
+        window.document.title = mensagem(inputNome.title);
+    };
+
+
+    window.document.body.appendChild(inputNome);
+    
+    // var nome = prompt("Digite seu nome");
+    // inputNome.value = msg(nome);
+
+    // var ano = prompt("Digite um ano");
+    // inputNome.value = anobissexto(ano);
+
+// console.log (Math.round (1 + Math.random() * 254));
+window.document.body.innerHTML = paletaCores (10,20);
+console.log(paletaCores(0,0));
+
+};
+
+
+
+
